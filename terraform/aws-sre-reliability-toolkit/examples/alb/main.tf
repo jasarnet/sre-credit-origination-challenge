781c820f30c189bb29b2a39ccd203a24a19f4c4f
jasarnet <50638806+jasarnet@users.noreply.github.com>
2025-12-29 16:21:01 -0600

    se agregan parametros en  main.tf

--- terraform/aws-sre-reliability-toolkit/examples/alb/main.tf
+++ terraform/aws-sre-reliability-toolkit/examples/alb/main.tf
@@ -1,14 +1,19 @@
 module "credit_origination_api_observability" {
   source = "../../modules/sre_api_observability"
 
-  service_name     = "credit-origination-api"
-  api_type         = "alb"
-  metric_namespace = "AWS/ApplicationELB"
+  ##############Identificación del servicio##############
+  service_name = "credit-origination-api"
+  api_type     = "alb"
 
+  ############### Métricas de CloudWatch para ALB ##############
+  metric_namespace = "AWS/ApplicationELB"
   sli_latency_metric = "TargetResponseTime"
   sli_error_metric   = "HTTPCode_Target_5XX_Count"
 
+  ############### Umbrales de SLIs
+  ############### Latencia promedio en segundos
   latency_threshold = 1
-  error_threshold   = 10
-}
 
+  ############### Número de errores 5XX permitidos por periodo
+  error_threshold = 10
+}
