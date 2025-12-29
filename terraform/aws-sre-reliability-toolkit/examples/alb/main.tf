57ea2a0a95f36f955d455a0c3e024d01134498f7
jasarnet <50638806+jasarnet@users.noreply.github.com>
2025-12-29 16:22:34 -0600

    Update main.tf

--- terraform/aws-sre-reliability-toolkit/examples/alb/main.tf
+++ terraform/aws-sre-reliability-toolkit/examples/alb/main.tf
@@ -1,19 +1,19 @@
 module "credit_origination_api_observability" {
   source = "../../modules/sre_api_observability"
 
-  ##############Identificación del servicio##############
-  service_name = "credit-origination-api"
-  api_type     = "alb"
+  ############## Identificación del servicio ##############
+  service_name         = "credit-origination-api"
+  api_type             = "alb"
 
   ############### Métricas de CloudWatch para ALB ##############
-  metric_namespace = "AWS/ApplicationELB"
-  sli_latency_metric = "TargetResponseTime"
-  sli_error_metric   = "HTTPCode_Target_5XX_Count"
+  metric_namespace     = "AWS/ApplicationELB"
+  sli_latency_metric   = "TargetResponseTime"
+  sli_error_metric     = "HTTPCode_Target_5XX_Count"
 
-  ############### Umbrales de SLIs
+  ############### Umbrales de SLIs##############
   ############### Latencia promedio en segundos
-  latency_threshold = 1
+  latency_threshold    = 1
 
-  ############### Número de errores 5XX permitidos por periodo
-  error_threshold = 10
+  ############### Número de errores 5XX permitidos por periodo##############
+  error_threshold      = 10
 }
