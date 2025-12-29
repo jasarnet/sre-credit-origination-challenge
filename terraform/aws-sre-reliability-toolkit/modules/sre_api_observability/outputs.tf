28061c1cb5aadcfe559a187209ee5a19ad17a913
jasarnet <50638806+jasarnet@users.noreply.github.com>
2025-12-29 16:18:08 -0600

    Update varibles de outputs.tf

--- terraform/aws-sre-reliability-toolkit/modules/sre_api_observability/outputs.tf
+++ terraform/aws-sre-reliability-toolkit/modules/sre_api_observability/outputs.tf
@@ -1,15 +1,14 @@
 output "dashboard_name" {
-  description = "SRE CloudWatch dashboard name"
+  description = "Nombre del dashboard de CloudWatch para SRE"
   value       = aws_cloudwatch_dashboard.sre_dashboard.dashboard_name
 }
 
 output "latency_sli_alarm" {
-  description = "Latency SLI alarm name"
+  description = "Nombre de la alarma del SLI de latencia"
   value       = aws_cloudwatch_metric_alarm.latency_sli.alarm_name
 }
 
 output "error_rate_sli_alarm" {
-  description = "Error rate SLI alarm name"
+  description = "Nombre de la alarma del SLI de tasa de errores"
   value       = aws_cloudwatch_metric_alarm.error_rate_sli.alarm_name
 }
-
