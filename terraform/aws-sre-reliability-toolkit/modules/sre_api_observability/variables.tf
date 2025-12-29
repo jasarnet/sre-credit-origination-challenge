f343842240c27ae54f3860288ebe665af7886ec6
jasarnet <50638806+jasarnet@users.noreply.github.com>
2025-12-29 16:19:29 -0600

    error en variables.tf

--- terraform/aws-sre-reliability-toolkit/modules/sre_api_observability/variables.tf
+++ terraform/aws-sre-reliability-toolkit/modules/sre_api_observability/variables.tf
@@ -1,42 +1,42 @@
 variable "service_name" {
-  description = "Name of the API or service"
+  description = "Nombre del servicio o API"
   type        = string
 }
 
 variable "api_type" {
-  description = "API type: alb or apigateway"
+  description = "Tipo de API: alb o apigateway"
   type        = string
 }
 
 variable "metric_namespace" {
-  description = "CloudWatch metric namespace"
+  description = "Namespace de métricas en CloudWatch"
   type        = string
 }
 
 variable "sli_latency_metric" {
-  description = "Latency SLI metric name"
+  description = "Nombre de la métrica del SLI de latencia"
   type        = string
 }
 
 variable "sli_error_metric" {
-  description = "Error rate SLI metric name"
+  description = "Nombre de la métrica del SLI de tasa de errores"
   type        = string
 }
 
 variable "latency_threshold" {
-  description = "Latency SLI threshold"
+  description = "Umbral del SLI de latencia"
   type        = number
   default     = 1000
 }
 
 variable "error_threshold" {
-  description = "Error rate SLI threshold"
+  description = "Umbral del SLI de tasa de errores"
   type        = number
   default     = 5
 }
 
 variable "alarm_actions" {
-  description = "SNS topic ARNs for notifications"
+  description = "Lista de ARNs de tópicos SNS para notificaciones"
   type        = list(string)
   default     = []
 }
