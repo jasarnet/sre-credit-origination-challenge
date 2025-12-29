5c847623b75f58401e1e6b5c0f7bbebf2b5e75ff
jasarnet <50638806+jasarnet@users.noreply.github.com>
2025-12-29 16:21:36 -0600

    Update main.tf

--- terraform/aws-sre-reliability-toolkit/examples/apigw/main.tf
+++ terraform/aws-sre-reliability-toolkit/examples/apigw/main.tf
@@ -1,14 +1,19 @@
 module "credit_origination_apigw_observability" {
   source = "../../modules/sre_api_observability"
 
-  service_name     = "credit-origination-apigw"
-  api_type         = "apigateway"
-  metric_namespace = "AWS/ApiGateway"
+  # Identificación del servicio
+  service_name = "credit-origination-apigw"
+  api_type     = "apigateway"
 
+  # Métricas de CloudWatch para API Gateway
+  metric_namespace   = "AWS/ApiGateway"
   sli_latency_metric = "Latency"
   sli_error_metric   = "5XXError"
 
+  # Umbrales de SLIs
+  # Latencia en milisegundos
   latency_threshold = 1000
-  error_threshold   = 5
-}
 
+  # Número de errores 5XX permitidos por periodo
+  error_threshold = 5
+}
